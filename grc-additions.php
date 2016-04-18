af4a6d2a917c333c5a458fb4b6487262ca7ac6ec
damonmaldonado <damon@mpwmarketing.com>
2016-04-18 16:20:49 -0400

    Update grc-additions.php

--- grc-additions.php
+++ grc-additions.php
@@ -15,8 +15,8 @@ add_shortcode ('clear', 'add_clear');
 function add_raw () {
 	return;
 }
-add_shortcode('raw' 'add_raw');
-}
+add_shortcode('raw', 'add_raw');
+
 function emma_signup_form (){
 	return '<link href="http://grcmechanical.com/wp-content/uploads/signup.med.css" rel="stylesheet" type="text/css">
 	<script type="text/javascript" src="https://app.e2ma.net/app2/audience/tts_signup/1770361/15166d5c89154c879fb5d866fa9708d7/43689/?v=a"></script><div id="load_check" class="signup_form_message" >This form needs Javascript to display, which your browser doesn\'t support. <a href="https://app.e2ma.net/app2/audience/signup/1770361/43689/?v=a"> Sign up here</a> instead </div><script type="text/javascript">signupFormObj.drawForm();</script>';}
